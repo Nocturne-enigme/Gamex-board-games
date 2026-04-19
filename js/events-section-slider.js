fc4fb287c886e91945c836a06facca18f3b24efc
Maryna Bilobrytska <bmv0288@gmail.com>
2026-04-19 03:57:13 +0300

    full adaptation

--- js/events-section-slider.js
+++ js/events-section-slider.js
@@ -2,14 +2,15 @@ const swiper = new Swiper(".swiper", {
   slidesPerView: 2,
   spaceBetween: 20,
   loop: true,
-  // breakpoints: {
-  //   768: {
-  //     slidesPerView: 2,
-  //   },
-  //   1280: {
-  //     slidesPerView: 3,
-  //   },
-  // },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    1040: {
+      slidesPerView: 2,
+    },
+  },
+  watchOverflow: true,
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
